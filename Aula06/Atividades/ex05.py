b1a216470cda364632f1e1fe247a91523524f903
Hugo Anísio <113561700+Hugarios@users.noreply.github.com>
2022-09-14 21:26:11 -0300

    Create ex05.py

---
+++ Aula06/Atividades/ex05.py
@@ -0,0 +1,9 @@
+lista_amigos = [
+    'Rafael',
+    'Leonardo',
+    'Donatello',
+    'Michelangelo']
+x = 0
+while x < len(lista_amigos):
+    print(f'Olá, como vai você {lista_amigos[x]}?')
+    x += 1
