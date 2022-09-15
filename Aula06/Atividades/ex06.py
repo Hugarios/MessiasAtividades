bd20bb5e94946d2f6f149a5c55228515d2f89812
Hugo Anísio <113561700+Hugarios@users.noreply.github.com>
2022-09-14 21:26:30 -0300

    Create ex06.py

---
+++ Aula06/Atividades/ex06.py
@@ -0,0 +1,24 @@
+lista_convidados = [
+    'Saitama',
+    'Genos',
+    'Garou',
+    'Bang',
+    'Tatsumaki']
+x = 0
+while x < len(lista_convidados):
+    print(f'Olá, {lista_convidados[x]}, você está convidado(a), para uma festa em minha residência!')
+    x += 1
+print()
+print('Tatsumaki não poderá comparecer.')
+print()
+lista_convidados = [
+    'Saitama',
+    'Genos',
+    'Garou',
+    'Bang',
+    'Blast']
+
+x = 0
+while x < len(lista_convidados):
+    print(f'Olá, {lista_convidados[x]}, você está convidado, para uma festa em minha casa!')
+    x += 1
